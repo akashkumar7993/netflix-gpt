8cd1b2c423df68411471167a6b3f363f50d3e7fd
akashkumar7993 <akumar7993@gmail.com>
2024-03-13 16:46:22 +0530

    components & react-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
-
+import Body from './components/Body';
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold text-red-600 "> hello everyone how are you!</h1>
-    </div>
+    <Body/>
   );
 }
 
