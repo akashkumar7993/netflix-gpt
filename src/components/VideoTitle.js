e74d1f678c0d68a6b839e68474c32ce5c8c835ac
akashkumar7993 <akumar7993@gmail.com>
2024-05-12 01:08:08 +0530

    trailer video updated

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -1,8 +1,15 @@
 import React from 'react'
 
-const VideoTitle = () => {
+const VideoTitle = ({title, overview}) => {
   return (
-    <div>VideoTitle</div>
+    <div className='pt-36 px-12'>
+      <h1 className='text-6xl font-bold'>{title}</h1> 
+      <p className='py-6 text-lg w-1/4'>{overview}</p>
+      <div>
+        <botton className="bg-gray-400 text-white p-3 px-12 text-xl bg-opacity-50 rounded-lg cursor-pointer">Play</botton>
+        <button className="bg-gray-400 text-white p-3 mx-2 px-12 text-xl bg-opacity-50 rounded-lg cursor-pointer">More Info</button>
+      </div>
+    </div>
   )
 }
 
--- src/hooks/useNowPlayingMovies.js
+++ src/hooks/useNowPlayingMovies.js
@@ -11,7 +11,6 @@ const useNowPlayingMovies = () => {
     );
     
     const json = await data.json();
-    console.log(json.results);
     dispatch(addNowPlayingMovies(json.results));
     };
   
