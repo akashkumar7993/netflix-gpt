b25bca807984f64abd1428fd91e5bb529e612d67
akashkumar7993 <akumar7993@gmail.com>
2024-05-13 18:52:09 +0530

    movie list updated

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -9,12 +9,9 @@ const SecondaryContainer = () => {
     <div className='bg-black'>
       <div className='-mt-36 relative z-90'>
       <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies}/>
-      <MovieList title={"Trending"} movies={movies.nowPlayingMovies}/>
-      <MovieList title={"Popular"} movies={movies.nowPlayingMovies}/>
-      <MovieList title={"Hollywood Movies"} movies={movies.nowPlayingMovies}/>
-      <MovieList title={"Comedies"} movies={movies.nowPlayingMovies}/>
-      <MovieList title={"Indian movies"} movies={movies.nowPlayingMovies}/>
-      <MovieList title={"Your Next Watch"} movies={movies.nowPlayingMovies}/>
+      <MovieList title={"Top Rated"} movies={movies.topRatedMovies}/>
+      <MovieList title={"Popular"} movies={movies.popularMovies}/>
+      <MovieList title={"Upcoming Movies"} movies={movies.upcomingMovies}/>
       </div>
     </div> 
     )
--- src/utils/movieSlice.js
+++ src/utils/movieSlice.js
@@ -6,17 +6,29 @@ const movieSlice = createSlice({
     initialState: {
         nowPlayingMovies: null,
         trailerVideo: null,
+        topRatedMovies: null,
+        popularMovies: null,
+        upcomingMovies: null,
     },
     reducers: {
-        addNowPlayingMovies: (state, action) =>{
-            state.nowPlayingMovies = action.payload;
-        },
         addTrailerVideo: (state, action) => {
             state.trailerVideo = action.payload;
         },
+        addNowPlayingMovies: (state, action) =>{
+            state.nowPlayingMovies = action.payload;
+        },
+        addTopRatedMovies: (state, action) => {
+            state.topRatedMovies = action.payload;
+          },
+          addPopularMovies: (state, action) => {
+            state.popularMovies = action.payload;
+          },
+          addUpcomingMovies: (state, action) => {
+            state.upcomingMovies = action.payload;
+          },
     },
 });
 
-export const { addNowPlayingMovies, addTrailerVideo} = movieSlice.actions;
+export const { addNowPlayingMovies, addTrailerVideo, addTopRatedMovies, addPopularMovies, addUpcomingMovies} = movieSlice.actions;
 
 export default movieSlice.reducer;
